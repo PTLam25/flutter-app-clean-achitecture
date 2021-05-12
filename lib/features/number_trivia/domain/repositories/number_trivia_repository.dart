5370010d8c0133921c9262e3d76d9beb51723f49
Pham Tung Lam <lam.famtung@gmail.com>
2021-05-12 14:18:44 +0300

    created NumberTriviaRepository interface in domain and GetConcreteNumberTrivia usecase

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_app_clean_achitecture/core/error/failures.dart';
+import 'package:flutter_app_clean_achitecture/features/number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  // интерфейс Repository в domain для реализация Repository в data
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
